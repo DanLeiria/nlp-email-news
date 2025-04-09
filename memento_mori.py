a9fcfa528f6ec3e460cbfd890ecbc85402d64c05
DanLeiria <daniel.h.leiria@gmail.com>
2025-04-09 15:29:21 +0200

    Concluded memento mori tracker

--- memento_mori.py
+++ memento_mori.py
@@ -4,11 +4,12 @@ from dateutil.relativedelta import relativedelta
 
 def life_tracker(birthday_date: str, expect_years: int):
     """
-    This function tracks the number of weeks
-    I expect to have left of the users life
+    This function tracks in percentage the remaining
+    life the user has left given his/hers birthday date
+    and expect (estimated) years to live.
 
     Parameters:
-        - birthday_date: birthday_date: As a string, e.g., "17/10/1997"
+        - birthday_date: As a string, e.g., "17/10/1997"
         - expect_years: Number of expected years to live
     """
 
@@ -18,19 +19,18 @@ def life_tracker(birthday_date: str, expect_years: int):
     # Get todays date
     today_date = datetime.today().date()
 
-    # Calculate the current number of weeks since birthday
-    days_diff = (today_date - origin_date).days
-    current_weeks = days_diff // 7
+    # Calculate the current number of days since birthday
+    current_diff = (today_date - origin_date).days
 
-    # Calculate total number of weeks (entire life)
+    # Calculate total number of days (entire expected life)
     final_date = origin_date + relativedelta(years=expect_years)
-    final_weeks = (final_date - origin_date).days // 7
+    total_diff = (final_date - origin_date).days
 
-    # Calculate diff of weeks
-    diff_weeks = final_weeks - current_weeks
+    # Calculate percentage of remaining life
+    pct_life = (total_diff - current_diff) / total_diff * 100
 
-    return f"You have {diff_weeks} weeks left"
+    return f"{pct_life:.0f}% life remaining"
 
 
 if __name__ == "__main__":
-    print(life_tracker(birthday_date="16/10/1996", expect_years=76))
+    print(life_tracker(birthday_date="16/02/2005", expect_years=76))
